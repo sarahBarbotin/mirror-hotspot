22435cdd0331567ef8a5b200b2d14163e91121dc
cedricFanelli <fanelli.cedric@protonmail.com>
2021-12-07 16:31:37 +0100

    c-chore-plugin- banner update et single spot template creation

--- public/content/themes/hotspot/partials/banner.tpl.php
+++ public/content/themes/hotspot/partials/banner.tpl.php
@@ -5,17 +5,7 @@
             <div class="col-lg-12">
                 <div class="breadcrumb_iner">
                     <div class="breadcrumb_iner_item text-left">
-                    <?php
-                    if(is_user_logged_in()) {
-                        $user = wp_get_current_user();
-                        
-                        echo "<h2>Profil</h2>";
-                    }
-                    else{
-                        echo '<h2>Liste d\'event ou de spots (dynamiser)</h2>';
-                    }
-                ?>
-                        
+                        <h2>Titre à dynamiser</h2>';
                         <p>home . blog</p>
                     </div>
                 </div>
--- public/content/themes/hotspot/partials/spots/detail-single-spot.tpl.php
+++ public/content/themes/hotspot/partials/spots/detail-single-spot.tpl.php
@@ -1,19 +1,98 @@
-<!-- tour details content css start-->
-<section class="tour_details_content section_padding">
+<?php
+$articleId = get_the_id();
+$hasImage = has_post_thumbnail($articleId);
+if ($hasImage) {
+    $imageURL = get_the_post_thumbnail_url();
+} else {
+    $imageURL = 'https://picsum.photos/300/200?random=1';
+}
+?>
+<section class="blog_area single-post-area my-5">
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-lg-6">
-                <div class="content_iner">
-                    <p>Waters make fish every without firmament saw had. Morning air subdue. Our Air very one. Whales grass is fish whales winged night yielding land creeping that seed appear were bearing.</p>
-                    <p>Waters make fish every without firmament saw had. Morning air subdue. Our Air very one. Whales grass is fish whales winged night yielding land creeping that seed appear were bearing.
-                    Their</p>
-                    <p>Waters make fish every without firmament saw had. Morning air subdue. Our Air very one. Whales grass is fish whales winged night yielding land creeping that seed appear were bearing.</p>
-                    <div class="tour_details_content_btn">
-                        <a href="#" class="btn_1">Book Ticket</a>
+            <div class="col-lg-12 posts-list">
+                <div class="single-post">
+                    <!-- Author -->
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="blog-author">
+                                <div class="media align-items-center">
+                                    <img src="<?= $imageURL; ?>" alt="">
+                                    <div class="media-body">
+                                        <a href="#">
+                                            <h4><?= get_the_title() ?></h4>
+                                        </a>
+                                        <p><i class="fas fa-swimmer"></i>
+                                            <?php
+                                            $spotAdress = get_field('level');
+                                            ?>
+                                        </p>
+                                        <p><i class="fas fa-map-pin"></i>
+                                            <?php
+                                            $surferCity = get_field('city');
+                                            echo $surferCity;
+                                            ?>
+                                        </p>
+                                        <p><i class="fas fa-map-marker-alt"></i>
+                                            <?php
+                                            $surferZipcode = get_field('zipcode');
+                                            echo $surferZipcode;
+                                            ?>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="d-none d-sm-block mb-5 pb-4">
+                                <div id="map" style="height: 480px;"></div>
+                                <script>
+                                    function initMap() {
+                                        var uluru = {
+                                            lat: -25.363,
+                                            lng: 131.044
+                                        };
+                                        var grayStyles = [{
+                                            featureType: "all",
+                                            stylers: [{
+                                                saturation: -90
+                                            }, {
+                                                lightness: 50
+                                            }]
+                                        }, {
+                                            elementType: 'labels.text.fill',
+                                            stylers: [{
+                                                color: '#ccdee9'
+                                            }]
+                                        }];
+                                        var map = new google.maps.Map(document.getElementById('map'), {
+                                            center: {
+                                                lat: -31.197,
+                                                lng: 150.744
+                                            },
+                                            zoom: 9,
+                                            styles: grayStyles,
+                                            scrollwheel: false
+                                        });
+                                    }
+                                </script>
+                                <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpfS1oRGreGSBU5HHjMmQ3o5NLw7VdJ6I&callback=initMap">
+                                </script>
+
+                            </div>
+                        </div>
                     </div>
+
+                    <!-- profile description -->
+                    <div class="quote-wrapper">
+                        <div class="quotes">
+                            <?= get_the_content(); ?>
+                        </div>
+                    </div>
+
+
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!-- tour details content css end-->
