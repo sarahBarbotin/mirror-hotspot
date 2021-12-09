6ef752e7a396d06c1272fa283120528ac9c671a2
cedricFanelli <fanelli.cedric@protonmail.com>
2021-12-09 17:36:26 +0100

    c-chore-archive-list-moving spot form to archive

--- public/content/themes/hotspot/partials/spots/spots-list.tpl.php
+++ public/content/themes/hotspot/partials/spots/spots-list.tpl.php
@@ -5,7 +5,7 @@ $args = array(
     'post_type' => 'spot',
     'posts_per_page' => 9
 );
-$the_query = new WP_Query( $args ); ?>
+$the_query = new WP_Query($args); ?>
 
 
 <section class="top_place section_padding">
@@ -14,56 +14,57 @@ $the_query = new WP_Query( $args ); ?>
             <div class="col-xl-6">
                 <div class="section_tittle text-center">
                     <h2>Spots</h2>
-                    <p>Waters make fish every without firmament saw had. Morning air subdue. Our. Air very one. Whales grass is fish whales winged.</p>
+                    <p>Retrouvez une sélection des spots de surfs les plus agréables créés par les surfers eux-mêmes</p>
                 </div>
             </div>
         </div>
         <div class="row">
 
-        <?php if ( $the_query->have_posts() ) : ?>
-            <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+            <?php if ($the_query->have_posts()) : ?>
+                <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
 
-            <?php
-                // Récupération des images thumbnail
-                $articleId = get_the_id();
-                $hasImage = has_post_thumbnail($articleId);
-                if($hasImage) {
-                    $imageURL = get_the_post_thumbnail_url();
-                }
-                else {
-                    $imageURL = 'https://picsum.photos/300/200?random=1';
-                }
+                    <?php
+                    // Récupération des images thumbnail
+                    $articleId = get_the_id();
+                    $hasImage = has_post_thumbnail($articleId);
+                    if ($hasImage) {
+                        $imageURL = get_the_post_thumbnail_url();
+                    } else {
+                        $imageURL = 'https://picsum.photos/300/200?random=1';
+                    }
 
-                // Récupération des taxonomies
-                $taxonomies = wp_get_post_terms( $post->ID, ['level','departement'] );
+                    // Récupération des taxonomies
+                    $taxonomies = wp_get_post_terms($post->ID, ['level', 'departement']);
 
-                //dump($the_query);
+                    //dump($the_query);
 
-                $fields = get_fields();
-            ?> 
+                    $fields = get_fields();
+                    ?>
 
 
-            <div class="col-lg-6 col-md-6">
-                <div class="single_place" style="background-color: #e6f4fe;">
-                    <img src="<?=$imageURL;?>" alt="">
-                    <div class="hover_Text d-flex align-items-end justify-content-between">
-                        <div class="hover_text_iner">
-                        <?php if(!empty($taxonomies)) {
-                                foreach($taxonomies as $taxonomy) {
-                                    echo '<div class="place_btn mr-1">'. $taxonomy->name;
-                                    echo '</div>';
-                                }
-                            } ?>
-                            <h3><?php the_title(); ?></h3>
-                            <p><?php 
-                                    if(!empty($fields['city'])){
+                    <div class="col-lg-6 col-md-6">
+                        <div class="single_place" style="background-color: #e6f4fe;">
+                            <picture>
+                                <img src="<?= $imageURL; ?>" alt="" class="img-fluid">
+                            </picture>
+                            <div class="hover_Text d-flex align-items-end justify-content-between">
+                                <div class="hover_text_iner">
+                                    <?php if (!empty($taxonomies)) {
+                                        foreach ($taxonomies as $taxonomy) {
+                                            echo '<div class="place_btn mr-1">' . $taxonomy->name;
+                                            echo '</div>';
+                                        }
+                                    } ?>
+                                    <h3><?php the_title(); ?></h3>
+                                    <p><?php
+                                        if (!empty($fields['city'])) {
 
-                                        echo $fields['city'];
-                                    } else {
-                                        echo '-';
-                                    }
-                                ?> </p>
-                            <!-- <div class="place_review">
+                                            echo $fields['city'];
+                                        } else {
+                                            echo '-';
+                                        }
+                                        ?> </p>
+                                    <!-- <div class="place_review">
                                 <a href="#"><i class="fas fa-star"></i></a>
                                 <a href="#"><i class="fas fa-star"></i></a>
                                 <a href="#"><i class="fas fa-star"></i></a>
@@ -71,20 +72,20 @@ $the_query = new WP_Query( $args ); ?>
                                 <a href="#"><i class="fas fa-star"></i></a>
                                 <span>(210 review)</span>
                             </div> -->
-                        </div>
-                        <div class="details_icon text-right">
-                        <a href="<?php echo  get_the_permalink();?>"><i class="ti-face-smile"></i></a>
+                                </div>
+                                <div class="details_icon text-right">
+                                    <a href="<?php echo  get_the_permalink(); ?>"><i class="ti-face-smile"></i></a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
 
-
-            <?php endwhile; ?>  
-            <?php wp_reset_postdata(); ?>
+                <?php endwhile; ?>
+                
             <?php endif; ?>
-            
+
         </div>
+        <?php get_template_part('partials/spots/spot-form.tpl'); ?>
     </div>
 </section>
-<!-- hotel list css end -->
+<!-- spot list css end -->
