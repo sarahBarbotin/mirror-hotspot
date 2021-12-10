c19f001db7e003b58ae0b67eaf02f59e7e929c67
GeoffreyLebon <geoffrey.lebon92@gmail.com>
2021-12-10 10:01:10 +0100

    g-feat admin bar disappears when not admin

--- public/content/themes/hotspot/functions.php
+++ public/content/themes/hotspot/functions.php
@@ -145,6 +145,14 @@ add_action(
     'hotspot_loadAssets'
 );
 
+add_action('after_setup_theme', 'remove_admin_bar');
+
+function remove_admin_bar() {
+if (!current_user_can('administrator') && !is_admin()) {
+  show_admin_bar(false);
+}
+}
+
 /**
  * Generate breadcrumbs
  * @author CodexWorld
