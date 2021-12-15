faad4f441043fc1da77904884c6c2a6cb5358b85
sarahBarbotin <sarah.barbotin@gmail.com>
2021-12-15 20:42:37 +0100

    s-chore-aside-searchbar comments

--- public/content/themes/hotspot/partials/aside/searchbar.tpl.php
+++ public/content/themes/hotspot/partials/aside/searchbar.tpl.php
@@ -2,15 +2,15 @@
     <form action="<?php echo esc_url( home_url( '/' ) ); ?>">
         <div class="form-group">
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder='Search Keyword'
+                <input type="text" class="form-control" placeholder='Recherche'
                     onfocus="this.placeholder = ''"
-                    onblur="this.placeholder = 'Search Keyword'" value="<?php echo get_search_query(); ?>" name="s">
+                    onblur="this.placeholder = 'Recherche'" value="<?php echo get_search_query(); ?>" name="s">
                 <div class="input-group-append">
                     <button class="btn" type="button"><i class="ti-search"></i></button>
                 </div>
             </div>
         </div>
         <button class="button rounded-0 primary-bg text-white w-100 btn_1"
-            type="submit" value="Recherche">Search</button>
+            type="submit" value="Recherche">Recherche</button>
     </form>
 </aside>
