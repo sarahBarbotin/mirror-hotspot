44130c64e9a24ffa1781ddb582a6bc46b2dcc1ae
sarahBarbotin <sarah.barbotin@gmail.com>
2021-12-16 12:08:32 +0100

    sql file

--- public/content/themes/hotspot/functions.php
+++ public/content/themes/hotspot/functions.php
@@ -126,13 +126,13 @@ add_action(
     'hotspot_loadAssets'
 );
 
-// add_action('after_setup_theme', 'remove_admin_bar');
+add_action('after_setup_theme', 'remove_admin_bar');
 
-// function remove_admin_bar() {
-// if (!current_user_can('administrator') && !is_admin()) {
-//   show_admin_bar(false);
-// }
-// }
+function remove_admin_bar() {
+if (!current_user_can('administrator') && !is_admin()) {
+  show_admin_bar(false);
+}
+}
 
 /**
  * Generate breadcrumbs
