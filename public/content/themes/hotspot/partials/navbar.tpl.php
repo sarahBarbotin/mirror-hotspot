2e3dff4e178bf044bd76c7acd6f5a77fe64c5cb4
cedricFanelli <fanelli.cedric@protonmail.com>
2021-12-20 16:47:08 +0100

    c-fix-navbar

--- public/content/themes/hotspot/partials/navbar.tpl.php
+++ public/content/themes/hotspot/partials/navbar.tpl.php
@@ -15,7 +15,6 @@
                         <div class="collapse navbar-collapse main-menu-item justify-content-center"
                             id="navbarSupportedContent">
                             <ul class="navbar-nav">
-
                                 <li class="nav-item">
                                     <a class="nav-link" href="<?= get_permalink( get_page_by_title( 'about'
                                     ) ); ?>">À propos</a>
@@ -36,34 +35,31 @@
                                     </span>
                                 </li>
                                 <?php
-                            if(!is_user_logged_in()) {
+                                    if(!is_user_logged_in()) {
 
-                                echo '<a href="' . wp_login_url() . '" class="btn_1 d-none d-lg-block ml-2">Connexion</a>';
-                                echo '<a href="'. wp_registration_url() .'" class="btn_2 d-none d-lg-block ml-2">Inscription</a>';
-                            }
-                            else {
-                                $user = wp_get_current_user();
-                                global $router;
-                                
-                                // TODO
-                                //route custom pour afficher le profil
-                                // echo '<li><a href="' . $url . '">' . $user->display_name . '</a></li>';
-                                $userID = get_current_user_id();
-                                $surferProfile = new WP_Query(
-                                    ['post_type' => 'surfer-profile',
-                                    'author' => $userID]
-                                );
+                                        echo '<li class="nav-item search-form mr-2"><a href="' . wp_login_url() . '" class="btn_1 search-form  ml-2">Connexion</a></li>';
+                                        echo '<li class="nav-item search-form mr-2"><a href="'. wp_registration_url() .'" class="btn_2 search-form  ml-2">Inscription</a></li>';
+                                    }
+                                    else {
+                                        $user = wp_get_current_user();
+                                        global $router;
+                                        
+                                        // TODO
+                                        //route custom pour afficher le profil
+                                        // echo '<li><a href="' . $url . '">' . $user->display_name . '</a></li>';
+                                        $userID = get_current_user_id();
+                                        $surferProfile = new WP_Query(
+                                            ['post_type' => 'surfer-profile',
+                                            'author' => $userID]
+                                        );
 
-                                echo '<a href="'. $surferProfile->posts[0]->guid .'" class="btn_1 d-none d-lg-block ml-2">Profil</a>';
-                                echo '<a href="'. wp_logout_url() .'" class="btn_2 d-none d-lg-block ml-2">Déconnexion</a>';
-                                
-                            }
-                        ?>
+                                        echo '<li class="nav-item search-form mr-2"><a href="'. $surferProfile->posts[0]->guid .'" class="btn_1 search-form d-lg-block ml-2">Profil</a></li>';
+                                        echo '<li class="nav-item search-form mr-2"><a href="'. wp_logout_url() .'" class="btn_2 search-form  ml-2">Déconnexion</a></li>';
+                                        
+                                    }
+                                ?>
                             </ul>
                         </div>
-
-
-
                     </nav>
                 </div>
             </div>
