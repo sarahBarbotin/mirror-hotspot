4add5a6525203d461b89b2cd5acd61acb7e35ecf
cedricFanelli <fanelli.cedric@protonmail.com>
2021-12-22 11:28:34 +0100

    c-fix-updating 404 page

--- public/content/themes/hotspot/404.php
+++ public/content/themes/hotspot/404.php
@@ -1,29 +1,33 @@
-
 <!doctype html>
-<html lang="<?=get_bloginfo('language');?>">
+<html lang="<?= get_bloginfo('language'); ?>">
 
 <head>
-<?php get_header(); ?>
+    <?php get_header(); ?>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Hotspot</title>
-    
+
 </head>
+
 <body>
 
     <!-- Header start-->
     <?php
-        get_template_part('partials/navbar.tpl');
+    get_template_part('partials/navbar.tpl');
     ?>
     <?php
-        get_template_part('partials/banner.tpl');
+    get_template_part('partials/banner.tpl');
     ?>
     <!-- Header end -->
 
     <section class="blog_area single-post-area section_padding">
         <div class="container">
-            <div>
+            <div class="section_tittle text-center">
+                <h2>Erreur 404</h2>
+                <p>Vous n'avez pas pris la bonne vague...</p>
+            </div>
+            <div class="text-center">
                 <img src="<?= get_theme_file_uri('assets/img/404.png'); ?>" alt="error 404 - surfer falling" class="img-fluid">
             </div>
             <div class="row justify-content-center my-3">
@@ -35,9 +39,9 @@
     </section>
 
 
-<!-- Footer start -->
-<?php
-        get_template_part('partials/footer.tpl');
+    <!-- Footer start -->
+    <?php
+    get_template_part('partials/footer.tpl');
     ?>
     <!-- Footer end -->
 
